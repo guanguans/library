6636a07affb2a508c37b62934512c90c11de9624
PHP-姚照明 <798314049@qq.com>
2018-03-31 09:31:08 +0800

    更新 Verify

--- tests/VerifyTest/VerifyForm.php
+++ tests/VerifyTest/VerifyForm.php
@@ -6,16 +6,18 @@
 	<link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-	<form action="VerifyCodeCheckTest.php" method="post" accept-charset="utf-8" class="col-lg-3">
-		<div class="form-group col-lg-9">
-			<img src="./VerifyCodeCreateTest.php" alt="验证码" width="100%" onClick="this.src=this.src+'?'+Math.random();">
-		</div>
-		<div class="form-group col-lg-9">
-			<input type="text" name="VerifyCode" class="form-control" placeholder="请输入验证码">
-		</div>
-	  	<div class="form-group col-lg-12">
-			<button type="submit" class="btn btn-primary">提交</button>
-		</div>
-	</form>
+	<div class="container">
+		<form action="VerifyCodeCheckTest.php" method="post" accept-charset="utf-8" class="col-lg-6">
+			<div class="form-group col-lg-9">
+				<img src="./VerifyCodeCreateTest.php" alt="验证码" width="100%" onClick="this.src=this.src+'?'+Math.random();">
+			</div>
+			<div class="form-group col-lg-9">
+				<input type="text" name="VerifyCode" class="form-control" placeholder="请输入验证码">
+			</div>
+		  	<div class="form-group col-lg-12">
+				<button type="submit" class="btn btn-primary">提交</button>
+			</div>
+		</form>
+	</div>
 </body>
 </html>
