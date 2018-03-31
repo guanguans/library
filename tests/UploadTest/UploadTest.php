7a2a03ad57a8a2b6891d8ac9c56885bcd840c582
PHP-姚照明 <798314049@qq.com>
2018-03-31 09:12:41 +0800

    更新 Upload

--- tests/UploadTest/UploadTest.php
+++ tests/UploadTest/UploadTest.php
@@ -8,27 +8,23 @@ error_reporting(E_ERROR);
 
 $config = array(
 	'maxSize'  => 3145728,
-	'rootPath' => './Uploads/',
-	'savePath' => '',
+	'rootPath' => 'uploads',// 设置附件上传根目录
+	'savePath' => 'img',// 设置附件上传（子）目录
 	'saveName' => array('uniqid',''),
 	'exts'     => array('jpg', 'gif', 'png', 'jpeg'),
 	'autoSub'  => true,
 	'subName'  => array('date','Ymd'),
 );
 
-$ftpConfig = array(        
+$ftpConfig = array(
     'host'     => 'bxu2713650276.my3w.com', // 服务器
     'port'     => 21,              // 端口
     'timeout'  => 90,              // 超时时间
-    'username' => 'username',      // 用户名
-    'password' => 'userpass',       // 密码
+    'username' => 'bxu2713650276',      // 用户名
+    'password' => 'yzm535287637',       // 密码
 );
 
-$upload = new Upload($config, 'ftp', $ftpConfig);// 实例化上传类
-$upload->maxSize   =     3145728 ;// 设置附件上传大小
-$upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-$upload->rootPath  =     'uploads/'; // 设置附件上传根目录
-$upload->savePath  =     'img/'; // 设置附件上传（子）目录
+$upload = new Upload($config);// 实例化上传类
 
 $info = $upload->upload();
 if($info) {
