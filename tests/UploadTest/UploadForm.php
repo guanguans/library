25275cbb34c76aaa853d11e4b4573ebb4889b0c8
PHP-姚照明 <798314049@qq.com>
2018-03-31 09:26:36 +0800

    更新 Upload

--- tests/UploadTest/UploadForm.php
+++ tests/UploadTest/UploadForm.php
@@ -6,21 +6,27 @@
 	<link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-	<form action="UploadTest.php" method="post" enctype="multipart/form-data">
-		<div class="form-group">
-			<label>单文件上传</label>
-			<input type="file" name="upload">
-		</div>
-		<button type="submit" class="btn btn-primary">Submit</button>
-	</form>
-	<hr>
-	<form action="UploadTest.php" method="post" enctype="multipart/form-data">
-		<div class="form-group">
-			<label>多文件上传</label>
-			<input type="file" name="upload[]">
-			<input type="file" name="upload[]">
-		</div>
-		<button type="submit" class="btn btn-primary">Submit</button>
-	</form>
+	<div class="container">
+		<form action="UploadTest.php" method="post" enctype="multipart/form-data">
+			<div class="form-group">
+				<label>单文件上传</label>
+				<input type="file" name="upload">
+			</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+		</form>
+		<hr>
+		<form action="UploadTest.php" method="post" enctype="multipart/form-data">
+			<div class="form-group">
+				<label>多文件上传</label>
+				<input type="file" name="upload[]">
+				<input type="file" name="upload[]">
+			</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+		</form>
+	</div>
 </body>
 </html>
